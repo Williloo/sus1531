d3df4b94dc21a658c530696e34ee18fb889dd298
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2025-02-26 10:37:01 +1100

    Added stub for adminAuthRegister and code comments for parameters, return types and description of the function

---
+++ src/auth.js
@@ -0,0 +1,25 @@
+
+/*
+* @param {string} email
+* @param {string} password
+* @param {string} nameFirst
+* @param {string} nameLast
+* @returns {Object}
+*/
+
+/*
+* @typdef {Object}
+* @property {number} userId
+*/
+
+/*
+* Register a user with an email, password and names 
+* then return their userId value.
+*/
+
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+  return {
+    userId: 1
+  }
+}
+
