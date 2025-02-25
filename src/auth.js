b16ab0c29e8e1ea143b484bf55485bf37c0a506c
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2025-02-26 10:41:26 +1100

    Completed function stub for adminAuthLogin and code comments including parameters, return types and description of the function

---
+++ src/auth.js
@@ -0,0 +1,21 @@
+/*
+* @param {string} email
+* @param {string} password
+* @returns {Object}
+*/
+
+/*
+* @typdef {Object}
+* @property {number} userId
+*/
+
+/*
+* Given a registered user's email and password, 
+* return their userId value.
+*/
+
+function adminAuthLogin(email, password) {
+  return {
+    userId: 1 
+  }
+}
