8b7dd5ca4c7eae67b9b747d17e8576730cac25c2
Susie Shen <z5593350@ad.unsw.edu.au>
2025-02-27 09:04:42 +1100

    Add function stub for adminQuizNameUpdate

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+/**
+ * Function to update the name of the relevant quiz.
+ * 
+ * @param {string} userId 
+ * @param {string} quizId 
+ * @param {string} name 
+ * @returns {object}
+ */
+function adminQuizNameUpdate(userId, quizId, name) {
+    return {};
+}
