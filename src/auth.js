7f825fdbc568849089dcd7e928a92be64141390f
Williloo <williloo456@gmail.com>
2025-02-26 22:15:26 +1100

    Created function stub for the function adminUserDetails

---
+++ src/auth.js
@@ -0,0 +1,11 @@
+function adminUserDetails( userId ) {
+    return { user:
+        {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+        }
+    }
+}
