63467f083c6dc6594d7a24eaede9b6b3f19bf702
William Liu <z5592167@ad.unsw.edu.au>
2025-02-26 22:45:46 +1100

    Created function comment for function adminUserPasswordUpdate

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,12 @@
+/**
+ * Given the userId of a user and their old password, updates the user's password from oldPassword
+ * to newPassword
+ * 
+ * @param { Number } userId 
+ * @param { String } oldPassword 
+ * @param { String } newPassword 
+ * @returns { Object }
+ */
 function adminUserPasswordUpdate( userId, oldPassword, newPassword ) {
     return { }
 }
