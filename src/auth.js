018c33366ab9d58508941c4397f6da8b45d540ed
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2025-02-27 09:54:07 +1100

    Updated code comments

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,16 @@
 
 /*
+* Register a user with an email, password and names 
+* then return their userId value.
+*
 * @param {string} email
 * @param {string} password
 * @param {string} nameFirst
 * @param {string} nameLast
 * @returns {Object}
-*/
-
-/*
+*
 * @typdef {Object}
 * @property {number} userId
-*/
-
-/*
-* Register a user with an email, password and names 
-* then return their userId value.
 */
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
