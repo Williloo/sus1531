c9957298d095683c4cb5dec0d12b44d7d79bbad6
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2025-02-27 09:49:20 +1100

    Updated code comments

--- src/auth.js
+++ src/auth.js
@@ -1,18 +1,15 @@
 /*
+* Given a registered user's email and password, 
+* return their userId value.
+*
 * @param {string} email
 * @param {string} password
 * @returns {Object}
-*/
-
-/*
+*
 * @typdef {Object}
 * @property {number} userId
 */
 
-/*
-* Given a registered user's email and password, 
-* return their userId value.
-*/
 
 function adminAuthLogin(email, password) {
   return {
