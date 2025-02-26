23d4c726ac80b382f7a5ca0ea5e7814b04357f83
Yash Mittal <z5624648@ad.unsw.edu.au>
2025-02-27 01:38:40 +1100

    Completed function stub for adminQuizCreate and code comments including parameters, return types and description of the function

---
+++ src/quiz.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {Integer} userId 
+ * @param {String} name 
+ * @param {String} description 
+ * @returns {object}
+ */
+
+/*
+ * This function takes in the userId , name and description
+ * Returns a quizId 
+ */
+
+function adminQuizCreate( userId, name, description ) {
+    return {
+        quizId: 2
+      }
+}
