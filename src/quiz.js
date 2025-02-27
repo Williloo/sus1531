f5e7332afbca233f394567f249fa101720041899
Haoyu Zhuang <z5395058@ad.unsw.edu.au>
2025-02-27 15:34:50 +1100

    added stub for adminQuizRemove function

---
+++ src/quiz.js
@@ -0,0 +1,12 @@
+/**
+ * <This function is used to permanently remove a quiz>
+ * 
+ * @param {number} unserId - this parameter is user id
+ * @param {number} quizId - this parameter is 
+ * 
+ * @returns {object} - this function returns an empty object
+ */
+
+function adminQuizRemove(userId, quizId) {
+    return {};
+}
