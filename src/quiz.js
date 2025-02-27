dc52ed6b249ea039b51541d605bcf112e25fd6ae
Haoyu Zhuang <z5395058@ad.unsw.edu.au>
2025-02-27 17:38:28 +1100

    fixed misspelling for userId parameter in description

--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,7 @@
 /**
  * <This function is used to permanently remove a particular quiz>
  * 
- * @param {number} unserId - this parameter is user id
+ * @param {number} userId - this parameter is user id
  * @param {number} quizId - this parameter is quiz id
  * 
  * @returns {object} - this function returns an empty object
