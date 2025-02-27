82023c380f242b689355ffbe90516d47abc0249c
Haoyu Zhuang <z5395058@ad.unsw.edu.au>
2025-02-27 15:40:43 +1100

    edited parameter description

--- src/quiz.js
+++ src/quiz.js
@@ -2,7 +2,7 @@
  * <This function is used to permanently remove a particular quiz>
  * 
  * @param {number} unserId - this parameter is user id
- * @param {number} quizId - this parameter is 
+ * @param {number} quizId - this parameter is quiz id
  * 
  * @returns {object} - this function returns an empty object
  */
