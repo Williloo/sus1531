a53a14cc8aa15e297106f5f61f4aea6f606323d4
Williloo <70116592+Williloo@users.noreply.github.com>
2025-02-28 15:12:22 +1100

    Editted whitespaces in quiz.js to be consistent with team

--- src/quiz.js
+++ src/quiz.js
@@ -1,106 +1,91 @@
 /** 
  * 
- * @param {number} userId 
- * @returns {Object}
+ * @param { number } userId 
+ * @returns { Object }
  * 
  * 
  * Given the userId 
  *  return a quiz object with quizId and name.
  */
-function adminQuizList(userId) {
-  return { 
-    quizzes: [
+function adminQuizList( userId ) {
+  return { quizzes: [
       {
         quizId: 1,
         name: 'My Quiz',
       }
     ]
-  };
+  }
 }
-
-
-
 
 /**
  * 
- * @param {number} userId 
- * @param {String} name 
- * @param {String} description 
- * @returns {object}
+ * @param { number } userId 
+ * @param { String } name 
+ * @param { String } description 
+ * @returns { object }
  * 
  * 
  * This function takes in the userId , name and description
  * Returns a quizId 
  */
-
 function adminQuizCreate( userId, name, description ) {
   return {
     quizId: 2
   }
 }
 
-
 /** 
  * <This function is used to permanently remove a particular quiz>
  * 
- * @param {number} userId - this parameter is user id
- * @param {number} quizId - this parameter is quiz id
+ * @param { number } userId - this parameter is user id
+ * @param { number } quizId - this parameter is quiz id
  * 
  * @returns {object} - this function returns an empty object
  */
-
-function adminQuizRemove(userId, quizId) {
-    return {};
+function adminQuizRemove( userId, quizId ) {
+  return {  };
 }
-
-
-
 
 /**
  * <This function gets all of the relevant information about current quiz>
  * 
- * @param {number} userId - user ID
- * @param {number} quizId - quiz ID
+ * @param { number } userId - user ID
+ * @param { number } quizId - quiz ID
  * 
- * @returns {object} - this function returns an object containing all
+ * @returns { object } - this function returns an object containing all
  * information properties about current quiz.
  * Example values are added to each property of the return object.
  */
-
-function adminQuizInfo (userId, quizId) {
-    return {
-        quizId: 1,
-        name: 'My Quiz',
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871,
-        description: 'This is my quiz',
-
-    }
+function adminQuizInfo ( userId, quizId ) {
+  return {
+    quizId: 1,
+    name: 'My Quiz',
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: 'This is my quiz',
+  }
 }
 
 /**
  * Function to update the name of the relevant quiz.
  * 
- * @param {number} userId - the ID of the user
- * @param {number} quizId - the ID of the quiz
- * @param {string} name - the name of the quiz that requires to be updated to
- * @returns {object}
+ * @param { number } userId - the ID of the user
+ * @param { number } quizId - the ID of the quiz
+ * @param { string } name - the name of the quiz that requires to be updated to
+ * @returns { object }
  */
-function adminQuizNameUpdate(userId, quizId, name) {
-  return {};
+function adminQuizNameUpdate( userId, quizId, name ) {
+  return {   };
 }
-
 
 /**
  * This function updates the description of the relevant quiz.
  * 
- * @param {number} userId - the ID of the user
- * @param {number} quizId - the ID of the quiz
- * @param {string} description - the quiz description that requires to be updated to
- * @returns {object}
+ * @param { number } userId - the ID of the user
+ * @param { number } quizId - the ID of the quiz
+ * @param { string } description - the quiz description that requires to be updated to
+ * @returns { object }
  */
-
-function adminQuizDescriptionUpdate (userId, quizId, description) {
-  return {};
+function adminQuizDescriptionUpdate( userId, quizId, description ) {
+  return {   };
 }
-
