d529cb9d276a0c44f5a1b0c4299f4b0aacab9455
Williloo <70116592+Williloo@users.noreply.github.com>
2025-02-28 15:02:23 +1100

    Editted the function comments of all functions in auth.js by adding information about the parameter and return variables

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,15 @@
-/*
+/**
 * Register a user with an email, password and names 
 * then return their userId value.
 *
-* @param { string } email
-* @param { string } password
-* @param { string } nameFirst
-* @param { string } nameLast
+* @param { string } email - The email fo the user being registered
+* @param { string } password - The password of the user being registered
+* @param { string } nameFirst - The first name of the user being registered
+* @param { string } nameLast - The last name of the user being registered
 * @returns { Object }
 *
 * @typdef { Object }
-* @property { number } userId
+* @property { number } userId - The user id of the user that has been registered
 */
 function adminAuthRegister( email, password, nameFirst, nameLast ) {
   return {
@@ -17,17 +17,16 @@ function adminAuthRegister( email, password, nameFirst, nameLast ) {
   }
 }
 
-
-/*
+/**
 * Given a registered user's email and password, 
 * return their userId value.
 *
-* @param { string } email
-* @param { string } password
-* @returns { Object }
+* @param { string } email - The email of the user logging in
+* @param { string } password - The password of the user logging in
+* @returns { Object } 
 *
 * @typdef { Object }
-* @property { number } userId
+* @property { number } userId - The userId of the user loogging in
 */
 function adminAuthLogin( email, password ) {
   return {
@@ -39,8 +38,17 @@ function adminAuthLogin( email, password ) {
  * This function takes an userId and returns information about the user
  * in an user object
  * 
- * @param { Number } userId 
+ * @param { number } userId - The Id of the user whose details are returned
  * @returns { Object }
+ * 
+ * @typedef { Object }
+ * @property { User } user - Object containing information about the user
+ * 
+ * @property { number } userId - The userId of the user
+ * @property { string } name - The user's first and last name concatenated with a single space between them
+ * @property { string } email - The email of the user
+ * @property { number } numSuccessfulLogins - Number of successful logins of the user
+ * @property { number } numFailedPasswordsSinceLastLogin - Number of failed password attempts of the user since the last login
  */
 function adminUserDetails( userId ) {
     return { user:
@@ -58,11 +66,11 @@ function adminUserDetails( userId ) {
  * Given a set of properties and the userId of an admin user, this function
  * updates those properties of the user
  * 
- * @param { Number } userId 
- * @param { String } email 
- * @param { String } nameFirst 
- * @param { String } nameLast 
- * @returns { Object }
+ * @param { number } userId - The userId of the user
+ * @param { string } email - The email of the user
+ * @param { string } nameFirst - The first name of the user
+ * @param { string } nameLast - The last name of the user
+ * @returns { Object } - Empty object
  */
 function adminUserDetailsUpdate( userId, email, nameFirst, nameLast ) {
     return { }
@@ -72,10 +80,10 @@ function adminUserDetailsUpdate( userId, email, nameFirst, nameLast ) {
  * Given the userId of a user and their old password, updates the user's password from oldPassword
  * to newPassword
  *
- * @param { Number } userId
- * @param { String } oldPassword
- * @param { String } newPassword
- * @returns { Object }
+ * @param { number } userId - The userId of the user
+ * @param { string } oldPassword - The old password of the user
+ * @param { string } newPassword - The new password of the user
+ * @returns { Object } - Empty object
  */
 function adminUserPasswordUpdate( userId, oldPassword, newPassword ) {
     return { }
