f39dd4bce4819f4179db7381dab509643fdadaf0
Williloo <70116592+Williloo@users.noreply.github.com>
2025-03-12 11:44:42 +1100

    Bugfixes for adminUserDetails tests

--- src/tests/adminUserDetails.test.js
+++ src/tests/adminUserDetails.test.js
@@ -9,24 +9,23 @@ import {
 } from '../auth.js'
 
 let user
-
 beforeEach(() => {
   clear()
-  user = adminAuthRegister( "123@gmail.com", "123", "my", "name" )
+  user = adminAuthRegister( "123@gmail.com", "password123", "my", "name" )
 })
 
 describe('tests for adminUserDetails', () => {
   test('invalid user id', () => {
     let invalidUid = user.userId + 1
 
-    expect(adminUserDetails(invalidUid).toStrictEqual(
+    expect(adminUserDetails(invalidUid)).toStrictEqual(
       { error: expect.any(String) }
-    ))
+    )
   })
 
   test('valid user id', () => {
     let uid = user.userId
-    expect(adminUserDetails(uid).toStrictEqual(
+    expect(adminUserDetails(uid)).toStrictEqual(
       { user: {
           userId: uid,
           name: "my name",
@@ -35,15 +34,15 @@ describe('tests for adminUserDetails', () => {
           numFailedPasswordsSinceLastLogin: 0,
         } 
       }
-    ))
+    )
   })
 
   test('multiple users', () => {
-    let user2 = adminAuthRegister( "456@gmail.com", "456", "your", "moniker" )
-    let uid1 = user1.userId
+    let user2 = adminAuthRegister( "456@gmail.com", "password456", "your", "moniker" )
+    let uid1 = user.userId
     let uid2 = user2.userId
 
-    expect(adminUserDetails(uid1).toStrictEqual(
+    expect(adminUserDetails(uid1)).toStrictEqual(
       { user: {
           userId: uid1,
           name: "my name",
@@ -52,9 +51,9 @@ describe('tests for adminUserDetails', () => {
           numFailedPasswordsSinceLastLogin: 0,
         } 
       }
-    ))
+    )
 
-    expect(adminUserDetails(uid2).toStrictEqual(
+    expect(adminUserDetails(uid2)).toStrictEqual(
       { user: {
           userId: uid2,
           name: "your moniker",
@@ -63,6 +62,6 @@ describe('tests for adminUserDetails', () => {
           numFailedPasswordsSinceLastLogin: 0,
         } 
       }
-    ))
+    )
   })
 })
