89662e8c8d67ccd091a8cfc34edff9e9fd0a590b
Haoyu Zhuang <z5395058@ad.unsw.edu.au>
2025-03-14 10:59:18 +1100

    added comments about this test to improve readablility

--- src/tests/adminQuizInfo.test.js
+++ src/tests/adminQuizInfo.test.js
@@ -11,6 +11,7 @@ import {
 let userId;
 let quizId;
 beforeEach('Success Register', () =>{
+    /*Used Input from adminAuthRegister.test.js to ensure a successfull return*/
     userId = adminAuthRegister(
         'jpozzolungo@gmail.com', 
         'thisisagoodpassword1974',
@@ -36,6 +37,8 @@ describe('tests for adminQuizInfo', () => {
     })
    
     test('Quiz ID Not Owned By Input User ID', () => {
+        /*create a quiz with a user id that is different from the userId 
+        that is about to be input in the adminQuizInfo function*/
         const anotherQuizId = adminQuizCreate(
             userId + 1, 
             'test_quiz', 
