70a436a6843271c386ee333f07e90be12bc8c85a
Haoyu Zhuang <z5395058@ad.unsw.edu.au>
2025-03-17 16:29:34 +1100

    Finished writing test for adminQuizRemove

---
+++ src/tests/adminQuizRemove.test.js
@@ -0,0 +1,57 @@
+import {
+    adminQuizRemove,
+} from '../quiz.js';
+import {
+    adminAuthRegister,
+} from '../auth.js';
+import {
+    adminQuizCreate,
+} from '../quiz.js';
+
+let userId;
+let quizId;
+beforeEach('Success Register', () =>{
+    /*Used Input from adminAuthRegister.test.js to ensure a successfull return*/
+    userId = adminAuthRegister(
+        'jpozzolungo@gmail.com', 
+        'thisisagoodpassword1974',
+        'Joshua', 
+        'Pozzolungo'
+    );
+    quizId = adminQuizCreate(
+        userId, 
+        'test_quiz', 
+        'This quiz is for testing adminQuizRemove function'
+    );
+});
+
+describe('tests for adminQuizRemove', () => {
+    test('Invalid User', () => {
+        expect(adminQuizRemove(userId + 1, quizId))
+        .toStrictEqual({error: 'Not A Valid User'});
+    })
+
+    test('Invalid Quiz', () => {
+        expect(adminQuizRemove(userId, quizId+1))
+        .toStrictEqual({error: 'Not A Valid Quiz'});
+    })
+   
+    test('Quiz ID Not Owned By Input User ID', () => {
+        /**create another user ID to successfully create another quiz ID*/
+        const newId = adminAuthRegister(
+            'Haoyuuuzz@gmail.com', 
+            'thisisagoodpassword2025',
+            'Haoyu', 
+            'Zhuang'
+        );
+        /*create a quiz with a user id that is different from the userId 
+        that is about to be input in the adminQuizRemove function*/
+        const anotherQuizId = adminQuizCreate(
+            newId, 
+            'test_quiz 2', 
+            'This quiz is for testing if the quiz Id is owned by input userid'
+        );
+        expect(adminQuizRemove(userId, anotherQuizId))
+        .toStrictEqual({error: 'Quiz Id not owned by this userId'});
+    })
+})
