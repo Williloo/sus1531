bb22a6429329ce7e33c1bd6e119d46ede33df67d
William Liu <z5592167@ad.unsw.edu.au>
2025-04-05 11:17:38 +1100

    new tests for adminQuestion functions

---
+++ src/tests/adminQuestionMove.test.ts
@@ -0,0 +1,49 @@
+describe('tests for adminQuestionMove', () => {
+  describe('success cases', () => {
+    test('successfully move questions', () => {
+
+    });
+
+    test('successfully moves questions when there are multiple quizzes and users', () => {
+
+    });
+  });
+
+  describe('error cases', () => {
+    test('empty session', () => {
+
+    });
+
+    test('invalid session', () => {
+
+    });
+
+    test('quiz doesn\'t exist', () => {
+
+    });
+
+    test('quiz isn\'t owned by user', () => {
+
+    });
+
+    test('questionId doesn\'t exist', () => {
+
+    });
+
+    test('questionId refers to question in another quiz', () => {
+
+    });
+
+    test('new position less than 0', () => {
+
+    });
+
+    test('new position longer than list', () => {
+
+    });
+
+    test('new position is current position', () => {
+
+    });
+  });
+});
